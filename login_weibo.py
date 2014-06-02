72758d1bf6d5ffcd1cf67fa89d7822144684e5be
xuxiaofeng <kent0630@gmail.com>
2014-06-02 08:21:51 +0800

    新增weibo模拟登陆

---
+++ login_weibo.py
@@ -0,0 +1,110 @@
+#! /usr/bin/env python
+#coding=utf8
+# 参考 http://www.douban.com/note/201767245/  
+#        http://www.cnblogs.com/mouse-coder/archive/2013/03/03/2941265.html
+import urllib
+import urllib2
+import cookielib
+import base64
+import re
+import json
+import hashlib
+import rsa
+import binascii
+
+cj = cookielib.LWPCookieJar()
+cookie_support = urllib2.HTTPCookieProcessor(cj)
+opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)
+urllib2.install_opener(opener)
+postdata = {
+    'entry': 'weibo',
+    'gateway': '1',
+    'from': '',
+    'savestate': '7',
+    'userticket': '1',
+    'ssosimplelogin': '1',
+    'vsnf': '1',
+    'vsnval': '',
+    'su': '',
+    'service': 'miniblog',
+    'servertime': '',
+    'nonce': '',
+    'pwencode': 'rsa2',
+    'sp': '',
+    'encoding': 'UTF-8',
+    'prelt': '115',
+    'rsakv': '',
+    'url': 'http://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack',
+    'returntype': 'META'
+}
+
+def get_servertime():
+    # url = 'http://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=dW5kZWZpbmVk&client=ssologin.js(v1.3.18)&_=1329806375939'
+    url = 'http://login.sina.com.cn/sso/prelogin.php?entry=sso&callback=sinaSSOController.preloginCallBack&su=%s&rsakt=mod&client=ssologin.js(v1.4.4)' 
+    data = urllib2.urlopen(url).read()
+    p = re.compile('\((.*)\)')
+    try:
+        json_data = p.search(data).group(1)
+        data = json.loads(json_data)
+        servertime = str(data['servertime'])
+        nonce = data['nonce']
+        pubkey = data['pubkey']
+        rsakv = data['rsakv']
+        print servertime
+        print nonce
+        print pubkey
+        print rsakv
+        return servertime, nonce, pubkey, rsakv
+    except:
+        print 'Get severtime error!'
+        return None
+
+def get_pwd(pwd, servertime, nonce, pubkey):
+    rsaPublickey = int(pubkey, 16)
+    key = rsa.PublicKey(rsaPublickey, 65537) #创建公钥
+    message = str(servertime) + '\t' + str(nonce) + '\n' + str(pwd) #拼接明文js加密文件中得到
+    passwd = rsa.encrypt(message, key) #加密
+    passwd = binascii.b2a_hex(passwd) #将加密信息转换为16进制。
+    return passwd
+
+
+
+def get_user(username):
+    username_ = urllib.quote(username)
+    username = base64.encodestring(username_)[:-1]
+    return username
+
+
+def login():
+    username = 'kent0630@sina.com'
+    pwd = 'XXFfvt6eip'
+    url = 'http://login.sina.com.cn/sso/login.php?client=ssologin.js'
+    try:
+        servertime, nonce, pubkey, rsakv = get_servertime()
+    except:
+        return
+    global postdata
+    postdata['servertime'] = servertime
+    postdata['nonce'] = nonce
+    postdata['su'] = get_user(username)
+    postdata['sp'] = get_pwd(pwd, servertime, nonce, pubkey)
+    postdata['rsakv'] = rsakv
+    postdata = urllib.urlencode(postdata)
+    headers = {'User-Agent':'Mozilla/5.0 (X11; Linux i686; rv:8.0) Gecko/20100101 Firefox/8.0'}
+    req = urllib2.Request(
+        url = url,
+        data = postdata,
+        headers = headers
+    )
+    result = urllib2.urlopen(req)
+    text = result.read()
+    p = re.compile('location\.replace\(\'(.*?)\'\)')
+    try:
+        login_url = p.search(text).group(1)
+        #print login_url
+        urllib2.urlopen(login_url)
+        print "登录成功!"
+    except:
+        print 'Login error!'
+
+login()
